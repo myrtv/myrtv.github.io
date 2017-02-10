d2b07403eff0d1c989e111f3b73c7653d71eda5e
RePod <RePod@users.noreply.github.com>
2017-02-10 06:25:16 -0500

    Maybe now.

--- js/site.js
+++ js/site.js
@@ -209,7 +209,7 @@ var rtv = {
                     var that = this;
 
                     var instance = $("<video />", {
-                        preload: "",
+                        preload: "none",
                         controls: "",
                         autoplay: "",
                         src: player.cache.info.url_prefix + current.qualities[0].src
