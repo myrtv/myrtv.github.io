2f54fa33027f3350f1e80f1c435290e4151247cd
RePod <RePod@users.noreply.github.com>
2017-06-27 21:42:58 -0400

    Vanilla Javascript YT to RTV playlist converter.

--- tools/parseyoutube.js
+++ tools/parseyoutube.js
@@ -1,16 +1,2 @@
-//Parsing a playlist. Must load all items manually.
-
-playlist = [];
-
-$(".pl-video.yt-uix-tile").each(function (index, item) {
-  var a = $(item).find(".timestamp span").text().split(':');
-  playlist.push({
-    "name": $(item).data("title"),
-    "duration": (+a[a.length-1])+(+a[a.length-2]*60)+((a.length==3)?(+a[a.length-3]*3600):0), //Why
-    "qualities": [{
-    "src": $(item).data("video-id")
-  }]
-  })
-})
-
-prompt('', JSON.stringify(playlist));
+//Vanilla Javascript Youtube to RTV playlist converter.
+var It=document.getElementsByClassName('pl-video yt-uix-tile'),n=document.getElementsByClassName('pl-header-title')[0].innerText,p={"info":{"name":n,"start_epoch_gtm":0,"end_epoch_gtm":0,"service":"youtube"},"playlist":[]};for(i=0;i<It.length;i++){var a=It[i].getElementsByClassName('timestamp')[0].getElementsByTagName('span')[0].innerText.split(':');p.playlist.push({"name":It[i].dataset.title,"duration":(+a[a.length-1])+(+a[a.length-2]*60)+((a.length==3)?(+a[a.length-3]*3600):0),"qualities":[{"src":It[i].dataset['videoId']}]})};prompt(name+'\n'+p.playlist.length+' videos',JSON.stringify(p));void(0)
