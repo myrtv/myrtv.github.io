0f27e3043135335ce0ddc57b003fe06c16f737cc
RePod <RePod@users.noreply.github.com>
2017-06-27 21:57:27 -0400

    The About window gets worse.
    But also embed the YT->RTV converter bookmarklet.

--- js/site.js
+++ js/site.js
@@ -8,7 +8,10 @@ var rtv = {
         this.init();
     },
     about: function() {
-        var about = "<div title='About RTV'><a href='https://github.com/myrtv/myrtv.github.io' target='_blank'>GitHub Repository</a> (submit bugs/requests here)<br><br>Made with jQuery and jQuery UI.</div>";
+        var about = '<div title="About RTV">'+
+                    '<a href="https://github.com/myrtv/myrtv.github.io" target="_blank">GitHub Repository</a> (submit bugs/requests here)'+
+                    '<br><br>Made with jQuery and jQuery UI.'+
+                    '<hr><a href=\'javascript:var T=document.getElementsByClassName("pl-video yt-uix-tile"),n=document.getElementsByClassName("pl-header-title")[0].innerText,p={"info":{"name":n,"start_epoch_gtm":0,"end_epoch_gtm":0,"service":"youtube"},"playlist":[]};for(i=0;i<T.length;i++){var a=T[i].getElementsByClassName("timestamp")[0].getElementsByTagName("span")[0].innerText.split(":");p.playlist.push({"name":T[i].dataset.title,"duration":(+a[a.length-1])+(+a[a.length-2]*60)+((a.length==3)?(+a[a.length-3]*3600):0),"qualities":[{"src":T[i].dataset["videoId"]}]})};prompt(name+"\n"+p.playlist.length+" videos",JSON.stringify(p));void(0)\'>YouTube to RTV Playlist Converter</a><br><small>Add as bookmark and activate on YouTube playlist pages.</small></div>';
 
         $(about).dialog({
             autoOpen: true,
