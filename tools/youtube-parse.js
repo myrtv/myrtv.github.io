dceecd2703f9ee4755db6fbadf2f5d815d386b49
RePod <RePod@users.noreply.github.com>
2017-11-30 11:53:49 -0500

    Daily rewrite

--- tools/youtube-parse.js
+++ tools/youtube-parse.js
@@ -1,2 +1,19 @@
-//JS YT->RTV playlist converter
-R=document.getElementsByClassName("pl-video"),T=document.getElementsByClassName("pl-header-title")[0].innerText,V={info:{name:T,service:"youtube"},playlist:[]};for(i=0;i<R.length;i++){S=R[i].getElementsByClassName("timestamp");if(!S.length)continue;a=S[0].innerText.split(":");V.playlist.push({name:R[i].dataset.title,duration:+a.pop()+(a.pop()*60)+(a[0]?a.pop()*3600:0),qualities:[{src:R[i].dataset.videoId}]})};Q=JSON.stringify(V);console.log(Q);prompt(T,Q);void(0)
+//JS YT->RTV playlist converter (new layout)
+R = document.getElementsByTagName("ytd-playlist-video-renderer"), T = document.getElementsByClassName("title")[0].innerText, V = {
+    info: {
+        name: T,
+        service: "youtube"
+    },
+    playlist: []
+};
+for (i = 0; i < R.length; i++) {
+    V.playlist.push({
+        name: R[i].__data__.data.title.simpleText,
+        duration: parseInt(R[i].__data__.data.lengthSeconds),
+        qualities: [{ src: R[i].__data__.data.videoId }]
+    })
+};
+Q = JSON.stringify(V);
+console.log(Q);
+prompt(T, Q);
+void(0)
