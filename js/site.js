75d641a040c1c7734ffa2fd925a14194e4723ce2
RePod <RePod@users.noreply.github.com>
2018-01-14 01:25:52 -0500

    Temp fix

--- js/site.js
+++ js/site.js
@@ -362,6 +362,9 @@ var rtv = {
                     $.each(this.cache.playlist, function (index, key) {
                         if (current_time < current_total + key.duration) {
                             the_key = key;
+                            if (!the_key.src) { //Temp until converted
+                                the_key.src = key.qualities[0].src
+                            }
                             the_key.seek_to = current_time - current_total;
                             return false;
                         } else {
